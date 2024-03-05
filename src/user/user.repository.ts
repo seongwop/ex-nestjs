f3333a8608984190bd2ecbb8d247c3c23bd73a5f
seongwop <psw921@gmail.com>
2024-03-05 23:29:02 +0900

    feat : 회원가입 관련 파일 생성

---
+++ src/user/user.repository.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+
+}
